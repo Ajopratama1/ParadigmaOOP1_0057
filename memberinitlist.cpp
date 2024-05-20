f4991d95da5317f1020fc32a4333e147b108d8ca
Ajopratama1 <a.ajopratama01@gmail.com>
2024-05-20 14:40:53 +0700

    membuat awalan coding dan class mahasiswa

---
+++ memberinitlist.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa {
+    private:
+    const int id;
+    string nama;
+    float nilai;
+    public:
+    Mahasiswa(int pId, string pNama, float pNilai) :id(pId),
+    nama(pNama), nilai(pNilai)
+    {
+
+    }
+
+  
